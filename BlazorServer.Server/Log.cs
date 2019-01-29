34023fbf602a47b448adcaf7761e730fc4877feb
chiisen <chiisen@hotmail.com>
2019-01-30 00:43:05 +0800

    多專案版本

---
+++ BlazorServer.Server/Log.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System.Diagnostics;
+using System.IO;
+
+namespace $safeprojectname$
+{
+    public class Log
+    {
+        public static string _logFileName = "";
+        static Logger _logger = null;
+
+        public static Logger SetupTheLogger
+        {
+            set { _logger = value; }
+        }
+
+        public static void Print(string msg)
+        {
+            // 新增 印出 程式碼的行號
+            StackFrame SF_ = new StackFrame(1, true);
+            string fileName_ = SF_.GetFileName();
+            string methodName_ = SF_.GetMethod().ToString();
+            int lineNumber_ = SF_.GetFileLineNumber();
+
+            string msg_ = string.Format("{0}({1}:{2})\n{3}", methodName_, Path.GetFileName(fileName_), lineNumber_, msg);
+
+            _logger.Warn(msg_);
+        }
+    }
+}
---
+++ BlazorServer.Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using System;
+using static $safeprojectname$.Log;
+
+namespace $safeprojectname$
+{
+    public class Program
+    {
+        // 設定 NLog 檔名
+        static void SetNlogFileName()
+        {
+            _logFileName = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            LogManager.Configuration.Variables["MY_DATE"] = _logFileName;
+        }
+
+        public static void Main(string[] args)
+        {
+            // NLog: setup the logger first to catch all errors
+            SetupTheLogger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+            SetNlogFileName();
+            try
+            {
+                Print("init main");
+                Print("Log File Name:" + _logFileName);
+
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                Print("Stopped program because of exception \n" + ex.ToString());
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(new ConfigurationBuilder()
+                        .AddCommandLine(args)
+                        .Build())
+                .UseUrls("http://*:5000")
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()  // NLog: setup NLog for Dependency injection
+                .Build();
+    }
+}
