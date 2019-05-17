4c02775d6f1c4a3fc594538afd71757650595d5f
chiisen <chiisen@hotmail.com>
2019-05-18 06:31:55 +0800

    新增 swagger

--- BlazorServer.Server/Startup.cs
+++ BlazorServer.Server/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
 using System.Linq;
 using System.Net.Mime;
 
@@ -15,6 +17,39 @@ namespace $safeprojectname$
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+			
+#region 註冊 Swagger
+            // 註冊 Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    // name: 攸關 SwaggerDocument 的 URL 位置。
+                    name: "v1",
+                    // info: 是用於 SwaggerDocument 版本資訊的顯示(內容非必填)。
+                    info: new Info
+                    {
+                        Title = "Web API",
+                        Version = "版本號：1.0.0",
+                        Description = "這是 ASP.NET Core 2.1 API",
+                        TermsOfService = "無",
+                        Contact = new Contact
+                        {
+                            Name = "John Wu",
+                            Url = "https://blog.johnwu.cc"
+                        },
+                        License = new License
+                        {
+                            Name = "CC BY-NC-SA 4.0",
+                            Url = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+                        }
+                    }
+                );
+                // 為 Swagger JSON and UI設置xml文檔註釋路徑
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//獲取應用程序所在目錄（絕對，不受工作目錄影響，建議採用此方法獲取路徑）
+                var xmlPath = Path.Combine(basePath, "Swagger.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+            #endregion 註冊 Swagger
 
             services.AddResponseCompression(options =>
             {
@@ -35,6 +70,20 @@ namespace $safeprojectname$
             {
                 app.UseDeveloperExceptionPage();
             }
+			
+			#region 註冊 Swagger
+            // 註冊 Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
+                    url: "/swagger/v1/swagger.json",
+                    // name: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
+                    name: "RESTful API v1.0.0"
+                );
+            });
+            #endregion 註冊 Swagger
 
             app.UseMvc(routes =>
             {
