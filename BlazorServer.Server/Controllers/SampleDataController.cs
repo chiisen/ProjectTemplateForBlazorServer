bed066bf848587f2673ec3a8c2f18d5c575b249e
chiisen <chiisen@hotmail.com>
2019-05-30 12:31:00 +0800

    1. 發行後執行批次檔 2. 依據組態讀取 appsettings.Debug.json 或 appsettings.Release.json

--- BlazorServer.Server/Controllers/SampleDataController.cs
+++ BlazorServer.Server/Controllers/SampleDataController.cs
@@ -1,20 +1,74 @@
 ﻿using BlazorServer.Shared;
 using Microsoft.AspNetCore.Mvc;
+using StackExchange.Profiling;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 
 namespace $safeprojectname$.Controllers
 {
+	/// <summary>
+    /// http://localhost:port/swagger 可以看到 swagger UI
+    /// </summary>
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
+		/// <summary>
+        /// http://localhost:port/profiler/results 可以看到 MiniProfiler
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string url1 = string.Empty;
+            string url2 = string.Empty;
+            using (MiniProfiler.Current.Step("Get方法"))
+            {
+                using (MiniProfiler.Current.Step("準備數據"))
+                {
+                    using (MiniProfiler.Current.CustomTiming("SQL", "SELECT * FROM Config"))
+                    {
+                        // 模擬一個SQL查詢
+                        Thread.Sleep(500);
+
+                        url1 = "https://www.baidu.com";
+                        url2 = "https://www.sina.com.cn/";
+                    }
+                }
+
+
+                using (MiniProfiler.Current.Step("使用從數據庫中查詢的數據，進行Http請求"))
+                {
+                    using (MiniProfiler.Current.CustomTiming("HTTP", "GET " + url1))
+                    {
+                        var client = new WebClient();
+                        var reply = client.DownloadString(url1);
+                    }
+
+                    using (MiniProfiler.Current.CustomTiming("HTTP", "GET " + url2))
+                    {
+                        var client = new WebClient();
+                        var reply = client.DownloadString(url2);
+                    }
+                }
+            }
+            return new string[] { "value1", "value2" };
+        }
+		
+		/// <summary>
+        /// 氣溫感受狀況
+        /// </summary>
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        /// <summary>
+        /// 取得氣溫資料列表
+        /// </summary>
+        /// <returns>回傳氣溫資料陣列給用戶</returns>
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
--- BlazorServer.Server/Startup.cs
+++ BlazorServer.Server/Startup.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
@@ -12,44 +12,43 @@ namespace $safeprojectname$
 {
     public class Startup
     {
+		public static IConfiguration JsonConfig { get; set; }
+		
+		public Startup(IConfiguration configuration)
+        {
+            // 讀取客制化 Json 檔案
+            // Json 檔案格為 appsettings.[目前組態].json
+            string appsettingJson_ = $"appsettings.{DebuggingProperties.Config}.json";
+
+            // 讀取目錄內客制化的 Json 檔案
+            JsonConfig = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(appsettingJson_, optional: true)
+            .Build();
+        }
+		
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 			
-#region 註冊 Swagger
-            // 註冊 Swagger
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc(
-                    // name: 攸關 SwaggerDocument 的 URL 位置。
-                    name: "v1",
-                    // info: 是用於 SwaggerDocument 版本資訊的顯示(內容非必填)。
-                    info: new Info
-                    {
-                        Title = "Web API",
-                        Version = "版本號：1.0.0",
-                        Description = "這是 ASP.NET Core 2.1 API",
-                        TermsOfService = "無",
-                        Contact = new Contact
-                        {
-                            Name = "John Wu",
-                            Url = "https://blog.johnwu.cc"
-                        },
-                        License = new License
-                        {
-                            Name = "CC BY-NC-SA 4.0",
-                            Url = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
-                        }
-                    }
-                );
-                // 為 Swagger JSON and UI設置xml文檔註釋路徑
-                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//獲取應用程序所在目錄（絕對，不受工作目錄影響，建議採用此方法獲取路徑）
-                var xmlPath = Path.Combine(basePath, "Swagger.xml");
-                c.IncludeXmlComments(xmlPath);
-            });
-            #endregion 註冊 Swagger
+
+			#region Register the MiniProfiler services
+
+            services.AddMiniProfiler(options => options.RouteBasePath = "/profiler");
+
+            #endregion Register the MiniProfiler services
+
+
+
+            #region Register the Swagger services
+
+            // Register the Swagger services
+            services.AddSwaggerDocument();
+
+            #endregion Register the Swagger services
+
 
             services.AddResponseCompression(options =>
             {
@@ -71,19 +70,23 @@ namespace $safeprojectname$
                 app.UseDeveloperExceptionPage();
             }
 			
-			#region 註冊 Swagger
-            // 註冊 Swagger
+
+			#region Register the MiniProfiler
+
+            app.UseMiniProfiler();
+
+            #endregion Register the MiniProfiler
+
+
+            #region Register the Swagger generator and the Swagger UI middlewares
+
+            // Register the Swagger generator and the Swagger UI middlewares
+
             app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint(
-                    // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
-                    url: "/swagger/v1/swagger.json",
-                    // name: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
-                    name: "RESTful API v1.0.0"
-                );
-            });
-            #endregion 註冊 Swagger
+            app.UseSwaggerUi3();
+
+            #endregion Register the Swagger generator and the Swagger UI middlewares
+
 
             app.UseMvc(routes =>
             {
